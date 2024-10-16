5c1d55354090643a3ea13a65465aee990d91b0b2
rafsyfaiz <43948389+rafsyfaiz@users.noreply.github.com>
2024-10-17 01:53:52 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const clearScreen = async () => {
+    document.getElementById("result").value = "";
+}
+
+const setScreenValue = async (value) => {
+  document.getElementById("result").value += value;
+}
